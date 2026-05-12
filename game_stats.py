6296f24e5f4d950904e929b67fcfe518db643805
rukenDogan <ruken.dogan.rkn@gmail.com>
2026-05-12 23:57:50 +0200

    update

--- game_stats.py
+++ game_stats.py
@@ -7,3 +7,4 @@ class GameStats:
     def reset_stats(self):
         """ Initialise les stats qui peuvent changer durant la partie """
         self.ships_left = self.settings.ship_limit # Nombre de vaisseaux (vies) que le joueur a au début de la partie
+        self.score = 0 # Score du joueur
