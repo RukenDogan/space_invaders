814de92575d4904d86de39adf1a53341461138a2
rukenDogan <ruken.dogan.rkn@gmail.com>
2026-05-13 14:29:24 +0200

    add: classe Scoreboard - gestion de l'affichage du score

---
+++ scoreboard.py
@@ -0,0 +1,15 @@
+import pygame.font
+
+class Scoreboard:
+    """Classe qui gère l'affichage du score"""
+    self.screen = ai_game.screen
+    self.screen_rect = self.screen.get_rect()
+    self.settings = ai_game.settings
+    self.stats = ai_game.stats
+
+    # Font settings for scoring information.
+    self.text_color = (30, 30, 30)
+    self.font = pygame.font.SysFont(None, 48)
+    
+    # Prepare the initial score image.
+    self.prep_score()
