58f12944d3a5b4a5cd45dfd176ebb7eb45fa0873
rukenDogan <ruken.dogan.rkn@gmail.com>
2026-06-02 09:25:38 +0200

    update: arrondir le score

--- scoreboard.py
+++ scoreboard.py
@@ -19,7 +19,8 @@ class Scoreboard:
 
     def prep_score(self):
         """Change le score en une image rendue"""
-        score_str = str(self.stats.score)
+        round_score = round(self.stats.score, -1)
+        score_str = f"{round_score:,}"
         self.score_image = self.font.render(score_str, True, self.text_color,
                                         self.settings.bg_color)
 
