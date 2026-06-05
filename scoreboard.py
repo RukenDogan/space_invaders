831d113315c3f93f42428c9bbd26f040bddb0513
rukenDogan <ruken.dogan.rkn@gmail.com>
2026-06-05 11:28:42 +0200

    add: méthode high_score

--- scoreboard.py
+++ scoreboard.py
@@ -34,3 +34,16 @@ class Scoreboard:
     def show_score(self):
         """Affiche le score à l'écran"""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+    
+    def prep_high_score(self):
+        """Change le high score en une image rendue"""
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = f"{high_score:,}"
+        self.high_score_image = self.font.render(high_score_str, True,
+            self.text_color, self.settings.bg_color)
+
+        # Center the high score at the top of the screen.
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
